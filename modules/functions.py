283d188d25ce63c931cb3e86cdb8a9560671447b
Lokraan <Lokraan@users.noreply.github.com>
2016-09-18 15:24:47 -0500

    added more documentation and find_links function
    find_links just grabs all the links on the page

--- modules/functions.py
+++ modules/functions.py
@@ -4,6 +4,16 @@ from bs4 import BeautifulSoup
 
 def get_content(url):
     source_code = requests.get(url)
+    
+    # Gets the source code of the page
+    # ex.
+    # <!DOCTYPE HTML>
+    # <html>
+    #    <body>
+    #        <p>blue</p>
+    #    </body>
+    # </html>
+
     return source_code.text
 
 
@@ -16,3 +26,15 @@ def page_info(url):
     plain_text = get_content(url)
     soup = BeautifulSoup(plain_text, 'html.parser')
     print(soup.find(id="page-description").text)
+
+
+# Finds all the links in the page
+def find_links(link):
+    soup = get_soup(link)
+    links = []
+    for link in soup.find_all('a'):
+        if link != '':
+            links.append(link.text)
+    print(links)
+# Way to to run
+# find_links("https://www.google.com")
